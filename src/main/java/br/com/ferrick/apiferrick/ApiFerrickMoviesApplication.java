f7fb2e53e788ca0dd22fd5916d6fbf07e69767bf
Ranon Emerick <ranonemerick@gmail.com>
2023-08-03 11:32:13 -0300

    First Commit Movie Project

---
+++ src/main/java/br/com/ferrick/apiferrick/ApiFerrickMoviesApplication.java
@@ -0,0 +1,13 @@
+package br.com.ferrick.apiferrick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFerrickMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFerrickMoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ferrick/apiferrick/ApiFerrickMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ferrick.apiferrick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFerrickMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
